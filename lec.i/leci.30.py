855be46bba01d47fc50208c2a1c7fb6d563348d0
Jack Tan <jiankemeng@gmail.com>
2020-03-11 21:27:51 +0800

    update lec.i

--- lec.i/leci.30.py
+++ lec.i/leci.30.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import pandas as pd
-from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
 
 t1 = pd.read_csv('../data/da02-temp-0948.csv', index_col='time', date_parser=lambda x: pd.to_datetime(float(x)+28800000000000))
 
@@ -9,12 +10,9 @@ t2 = pd.read_csv('../data/da02-temp-0019.csv', index_col='time', date_parser=lam
 
 t3 = pd.read_csv('../data/da02-temp-1144.csv', index_col='time', date_parser=lambda x: pd.to_datetime(float(x)+28800000000000))
 
-print(stats.pearsonr(t1['Temp'], t2['Temp']))
-#(0.6645110453498703, 2.134012282722948e-09)
+plt.plot(t1.index, t1['Temp'], label='t1')
+plt.plot(t2.index, t2['Temp'], label='t2')
+plt.plot(t3.index, t3['Temp'], label='t3')
 
-print(stats.pearsonr(t1['Temp'], t3))
-#(0.057569362179313834, 0.6513750312139728)
-
-print(stats.pearsonr(t2['Temp'], t3))
-#(0.07144298994314577, 0.5747996511588451)
-
+plt.legend()
+plt.show()
