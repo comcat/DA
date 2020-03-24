1b29cb57c791938d11ffc1369ded3103a512ec8d
Jack Tan <jiankemeng@gmail.com>
2020-03-24 13:31:34 +0800

    add a new us-vs-cn.py

---
+++ us-vs-cn.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
+
+from mpl_toolkits.axes_grid1 import host_subplot
+
+plt.rcParams['legend.fontsize'] = 'x-large'
+plt.rcParams['ytick.labelsize'] = 13
+
+plt.figure(figsize=(14.4,21.6))
+#plt.figure(figsize=(11.25,24.36))
+ 
+cn = pd.read_csv("./data/ncp-cn-new.csv", skipinitialspace=True, index_col='Date', parse_dates=True)
+us = pd.read_csv("./data/us.csv", skipinitialspace=True, index_col='Date', parse_dates=True)
+
+cnn = cn['Confirmed']
+usn = us['New Confirmed']
+
+cnn['2020-02-12'] = 1508	# Lab-confirmed cases, filter the Clinically diagnosed (exception number)
+
+cnn = cnn[:-1]		# start at 2020-01-10 (1/15) vs. start at 2020-02-29 (3/5)
+itn = usn[35:]
+cn_start_day = '2020-01-10'
+it_start_day = '2020-02-27'
+
+hl = len(cnn)
+cnn.index = np.arange(hl-1, -1, -1)
+
+il = len(itn)
+itn.index = np.arange(0, il, 1)
+
+xtk = np.arange(0, hl, 5)
+cn_day_range = pd.date_range(cn_start_day, periods=hl+5, freq='1d')
+xlb = cn_day_range.strftime('%m-%d')
+ii = []
+for i in xtk:
+	ii.append(xlb.values[i])
+
+it_day_range = pd.date_range(it_start_day, periods=il, freq='1d')
+it_start_day = it_day_range[0].strftime('%m/%d')
+it_end_day = it_day_range[il-1].strftime('%m/%d')
+
+cn_start_day = cn_day_range[0].strftime('%m/%d')
+cn_end_day = cn_day_range[il-1].strftime('%m/%d')
+
+#################################
+host = host_subplot(111)
+par = host.twinx()
+
+plt.title('New confirmed cases of COVID-19 (United States vs. China)\n', {'fontsize':17, 'fontweight':'bold','color': '#0b0c0c', 'verticalalignment':'top'})
+
+#bns=np.arange(-0.5,l+0.5,1)
+#par.hist(x, bins=bns, rwidth=0.9, alpha=0.4)
+w = 0.5
+# C1, C8 #A7C957
+par.bar(cnn.index, cnn.values, width=0.5, alpha=0.3, color='#1d70b8', label='China: '+cn_start_day+'~'+cn_end_day)
+par.bar(itn.index+w, itn.values, width=w, alpha=1, color='#d5281b', label='   USA: '+it_start_day+'~'+it_end_day)
+
+
+x = cnn.index.repeat(cnn)
+h1 = len(x)**(-1.0/5.0)
+h2 = 1.06 * np.std(x) * len(x)**(-1.0/5.0) - 0.79
+px = pd.Series(x)
+#px.plot.kde(bw_method=h1, color='C1', label='KDE (h='+str(format(h1,'.2f'))+')', linewidth=0.9, alpha=0.4)
+px.plot.kde(bw_method=h2, color='#DFDFE3', label='KDE (h='+str(format(h2,'.2f'))+')', linewidth=0.9, alpha=0.3)
+
+# 0.09/5k * 9k
+#host.set_ylim(ymin=0, ymax=0.16)
+
+host.set_ylim(ymin=0, ymax=0.09)
+
+host.set_ylabel('Probability (New / Total)',{'fontsize':14})
+
+par.set_ylabel('Number of cases',{'fontsize':14})
+
+plt.xlim(xmin=0, xmax=65)
+
+ax = plt.gca()
+ax.set_xticks(xtk)
+ax.set_xticklabels(ii,{'fontsize':13})
+
+ax.annotate('Data Source: The COVID Tracking Project, WHO, China CDC.  Analysis by: Jack Tan',
+			xy=(1, 0), xycoords='axes fraction',
+            xytext=(0, -116), textcoords='offset pixels',
+            horizontalalignment='right',
+            verticalalignment='bottom',
+			fontsize=14)
+
+plt.gcf().autofmt_xdate()
+
+# rect = (left,bottom,right,top)
+plt.tight_layout(pad=1.6, rect=(0.03,-0.03,0.97,0.96))
+
+plt.legend()
+plt.grid(linewidth=0.5,alpha=0.6)
+
+ofname = "export/us-vs-cn-" + it_day_range[il-1].strftime('%m%d') + '.png'
+
+plt.savefig(ofname, dpi=120)
+#plt.show()
