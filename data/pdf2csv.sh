2940fff56cfc742a3cb2aaec5aa1fab670eef4c6
Jack Tan <jiankemeng@gmail.com>
2020-03-24 13:31:53 +0800

    add the pdf2csv.sh

---
+++ data/pdf2csv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+td=$(date +%Y%m%d)
+day=$((td-1))
+
+pdf=../../../Documents/COVID-19/$day-*.pdf
+csv=covid-19-who/$day-outside-cn.csv
+
+echo $pdf
+echo $csv
+
+../pdf2csv.py -i $pdf  -o $csv -s 1 -n 5
